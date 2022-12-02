a3543e334a440fd9586b22b2ff55630f85ed2237
dovagneau <mathis.coiraton@gmail.com>
2022-12-02 15:43:39 +0100

    adding back

---
+++ testMogenius/src/main/java/com/polytech/testMogenius/TestMogeniusApplication.java
@@ -0,0 +1,13 @@
+package com.polytech.testMogenius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMogeniusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMogeniusApplication.class, args);
+	}
+
+}
---
+++ testMogenius/src/test/java/com/polytech/testMogenius/TestMogeniusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polytech.testMogenius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMogeniusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
